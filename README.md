f932a86d9a0f617a5ae0a4dec4b85a14ffe5627c
The Sharps <Sharp@Thes-Air.attlocal.net>
2018-03-10 20:53:40 -0500

    @HostListener

--- README.md
+++ README.md
@@ -26,4 +26,4 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
 # directives_angular
-Using the renderer to build a better attribute directive
+using HostListener to listen to host events
--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,27 +1,26 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+
 
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
+  
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
-  //we can't pass the reference itself
-  //we need to get access to the underlying element
-  //then pass that as it's first element here
-  //setStyle takes a couple of arguments here
-  //1. define what style we want to set 
-  //2. the value we want to assign for this property
-  //3. flags object (it is optional), things like !important
   ngOnInit() {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+  }
+//we place HostListener under ngOnInit so that this can be triggered
+//whenever some event occurs, which is specified as an argument
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
   }
 
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+  }
 }
-//why is renderer a better approach?
-//angular is not limited to running in the browser
-//it also works with service workers
-//these are environments where you might not have access to the DOM
-//in access the native style of this element, you may get an error in some cases
+
