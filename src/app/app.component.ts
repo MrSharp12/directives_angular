02ae2986290102d9f07921dd795d19e54397f210
The Sharps <Sharp@Thes-Air.attlocal.net>
2018-03-10 22:37:52 -0500

    ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-//not the most elegant way
-//set both of the number arrays
+
 export class AppComponent {
   // numbers = [1, 2, 3, 4, 5];
   oddNumbers = [1, 3, 5];
   evenNumbers = [2, 4];
   onlyOdd = false;
+  value = 5;
 }
